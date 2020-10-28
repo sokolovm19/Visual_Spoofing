3bfd70bcfa48efdbf36d284dbdbc3c6792d44433
sokolovm19 <57239639+sokolovm19@users.noreply.github.com>
2020-10-28 17:58:57 -0400

    Update pre-process.py

--- pre-process.py
+++ pre-process.py
@@ -3,3 +3,5 @@ ham = pd.read_csv("ham.txt",delimiter='\t+',header=None)
 ham.insert(0, 'label', '0')
 spam = pd.read_csv("spam.txt",delimiter='\t+',header=None) 
 spam.insert(0, 'label', '1')
+frames = [ham, spam]
+result = pd.concat(frames)
