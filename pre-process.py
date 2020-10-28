06d894bbf660d56ec348685a7fd93ace82485146
sokolovm19 <57239639+sokolovm19@users.noreply.github.com>
2020-10-28 17:23:14 -0400

    Update pre-process.py

--- pre-process.py
+++ pre-process.py
@@ -1,3 +1,21 @@
-import  pandas as pd
+import os
+from shutil import copyfile
+import random
 
-df = pd.read_csv(filename, sep=',')
+abc = ['ham','spam']
+for x in abc:
+    for y in range(6):
+        path = ''
+        percent_70 = (0.7)*len(os.listdir(path))
+        a = []
+        for j in range(len(os.listdir(path))):
+            a.append(j)
+        b = random.sample(a, int(percent_70))
+        for i in b:
+            copyfile(path+'/'+os.listdir(path)[i], ''+os.listdir(path)[i])
+        for v in range(len(os.listdir(path))):
+            c = os.listdir(path)[v]
+            train_path = ''
+            h = os.listdir(train_path)
+            if c not in h:
+                copyfile(path+'/'+c, ''+c)
